84ad1027bc1fbf21baf090aa1cc7e89ccf3b45f6
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2020-01-02 19:36:03 -0300

    Add header to merge gradient and card image list

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:trips/review_list.dart';
 import 'description_place.dart';
 import 'gradient_back.dart';
+import 'header_appbar.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.transparent, //<--NavigationBarColor
+    statusBarColor: Colors.transparent, //<--StatusBarColor
+  ));
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -15,14 +23,15 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
           body: Stack(children: <Widget>[
-            ListView(
-              children: <Widget>[
-                DescriptionPlace("Bahamas", 4, "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando"),
-                ReviewList()
-              ],
-            ),
-            GradientBack()
-          ])),
+        ListView(
+          children: <Widget>[
+            DescriptionPlace("Bahamas", 4,
+                "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando"),
+            ReviewList()
+          ],
+        ),
+        HeaderAppbar()
+      ])),
     );
   }
 }
